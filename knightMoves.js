b3fa2b576f013549a2d40a1bc9ff553d4004712b
Brian Wong <brianw7218@gmail.com>
2023-01-02 18:31:07 -0800

    Change parameters and add new pseudocode

--- knightMoves.js
+++ knightMoves.js
@@ -1,8 +1,16 @@
-const knightMoves = (startingCoordinate, endingCoordinate, moveCounter = 0) => {
+const knightMoves = (
+  startingCoordinate,
+  endingCoordinate,
+  moveCounter = 0,
+  visitedCoordinates = [],
+  queue = [startingCoordinate]
+) => {
   // print startingCoordinate
+  // use breadth first search
   // compare if startedCoordinate and endingCoordinate are the same
   // if not the same
   // determine the moves the knight can make without moving off board (8 possible permutations of x +- 1, y +- 1, 0 <= x <= 7, 0 <= y <= 7)
+  // recurvisely traverse the graph by calling knightMoves() with startingCoordinate being the possible moves
   // if the same
   // print moveCounter
   // return
