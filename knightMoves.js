4f664d5172dd6f9b6baaa7b893125d656bd893ed
Brian Wong <brianw7218@gmail.com>
2023-01-02 16:29:44 -0800

    Initialize file and write some pseudocode

---
+++ knightMoves.js
@@ -0,0 +1,9 @@
+const knightMoves = (startingCoordinate, endingCoordinate, moveCounter = 0) => {
+  // print startingCoordinate
+  // compare if startedCoordinate and endingCoordinate are the same
+  // if not the same
+  // determine the moves the knight can make without moving off board (8 possible permutations of x +- 1, y +- 1, 0 <= x <= 7, 0 <= y <= 7)
+  // if the same
+  // print moveCounter
+  // return
+};
