354d7a084a6fcf9490aba70dddd80336816dc38f
Brian Wong <brianw7218@gmail.com>
2023-01-03 19:08:41 -0800

    Convert back to breadth first search as that seems to be the correct approach

--- knightMoves.js
+++ knightMoves.js
@@ -10,10 +10,8 @@ const knightMoves = (
   currentMoveCounter = 0,
   lowestMoveCounter = Infinity,
   currentPath = [],
-  shortestPath = [startingCoordinate]
+  shortestPath = []
 ) => {
-  console.log(shortestPath) /* dont add to traversal queue is coordinate is already in queue */
-  // return error message if either coordinate is off the board/invalid
   if (
     startingCoordinate[0] < 0 ||
     startingCoordinate[0] > 7 ||
@@ -35,23 +33,22 @@ const knightMoves = (
     currentPath.push(startingCoordinate)
   }
   // remove first item in queue since it was just evaluated
-  // shortestPath.shift() /* removing for depth first */
+  shortestPath.shift() /* removing for depth first */
   // use breadth first search /* ACTUALLY maybe depth first would work better */
 
   // compare if startedCoordinate and endingCoordinate are the same
   if (startingCoordinate[0] === endingCoordinate[0] && startingCoordinate[1] === endingCoordinate[1]) {
-    console.log(startingCoordinate, "winner")
     // for breadth first
-    /* console.log(`You made it in ${currentMoveCounter} moves! Here's your path:`)
-      visitedCoordinates.forEach((coordinate) => {
-        console.log(coordinate)
-      }) */
+    console.log(`You made it in ${currentMoveCounter} moves! Here's your path:`)
+    currentPath.forEach((coordinate) => {
+      console.log(coordinate)
+    })
 
     // for depth first
-    if (currentMoveCounter < lowestMoveCounter) {
+    /* if (currentMoveCounter < lowestMoveCounter) {
       lowestMoveCounter = currentMoveCounter
       shortestPath = currentPath
-    }
+    } */
     // return [lowestMoveCounter, shortestPath]
     
     // if the same, means found the endingCoordinate and can display relevant info
@@ -63,199 +60,201 @@ const knightMoves = (
     if (
       startingCoordinate[0] + 2 <= 7 &&
       startingCoordinate[1] - 1 >= 0 &&
-      checkIfVisited(currentPath, [startingCoordinate[0] + 2, startingCoordinate[1] - 1]) === false /* &&
-      checkIfVisited(shortestPath, [startingCoordinate[0] + 2, startingCoordinate[1] - 1]) === false */
+      checkIfVisited(currentPath, [startingCoordinate[0] + 2, startingCoordinate[1] - 1]) === false &&
+      checkIfVisited(shortestPath, [startingCoordinate[0] + 2, startingCoordinate[1] - 1]) === false
     ) {
       // breadth first
-      /* if (endingCoordinate[0] === startingCoordinate[0] + 2 && endingCoordinate[1] === startingCoordinate[1] - 1) {
+      if (endingCoordinate[0] === startingCoordinate[0] + 2 && endingCoordinate[1] === startingCoordinate[1] - 1) {
         shortestPath.unshift([startingCoordinate[0] + 2, startingCoordinate[1] - 1])
         startingCoordinate = shortestPath[0]
         currentMoveCounter += 1
         return knightMoves(startingCoordinate, endingCoordinate, currentMoveCounter, lowestMoveCounter, currentPath, shortestPath)
       } 
-      shortestPath.push([startingCoordinate[0] + 2, startingCoordinate[1] - 1]) */
+      shortestPath.push([startingCoordinate[0] + 2, startingCoordinate[1] - 1])
 
       // depth first
-      const childCoordinate = [startingCoordinate[0] + 2, startingCoordinate[1] - 1]
+      /* const childCoordinate = [startingCoordinate[0] + 2, startingCoordinate[1] - 1]
       console.log(childCoordinate, "dwwd")
       const returnValues = knightMoves(childCoordinate, endingCoordinate, currentMoveCounter + 1, lowestMoveCounter, currentPath, shortestPath)
       console.log(returnValues, "wdwd")
       lowestMoveCounter = returnValues[0]
-      shortestPath = returnValues[1]
+      shortestPath = returnValues[1] */
     }
     // check if right 2, down 1 is valid and not already visited
     if (
       startingCoordinate[0] + 2 <= 7 &&
       startingCoordinate[1] + 1 <= 7 &&
-      checkIfVisited(currentPath, [startingCoordinate[0] + 2, startingCoordinate[1] + 1]) === false /* &&
-      checkIfVisited(shortestPath, [startingCoordinate[0] + 2, startingCoordinate[1] + 1]) === false */
+      checkIfVisited(currentPath, [startingCoordinate[0] + 2, startingCoordinate[1] + 1]) === false &&
+      checkIfVisited(shortestPath, [startingCoordinate[0] + 2, startingCoordinate[1] + 1]) === false
     ) {
       // breadth first
-      /* if (endingCoordinate[0] === startingCoordinate[0] + 2 && endingCoordinate[1] === startingCoordinate[1] + 1) {
+      if (endingCoordinate[0] === startingCoordinate[0] + 2 && endingCoordinate[1] === startingCoordinate[1] + 1) {
         shortestPath.unshift([startingCoordinate[0] + 2, startingCoordinate[1] + 1])
         startingCoordinate = shortestPath[0]
         currentMoveCounter += 1
         return knightMoves(startingCoordinate, endingCoordinate, currentMoveCounter, lowestMoveCounter, currentPath, shortestPath)
       } 
-      shortestPath.push([startingCoordinate[0] + 2, startingCoordinate[1] + 1]) */
+      shortestPath.push([startingCoordinate[0] + 2, startingCoordinate[1] + 1])
 
       // depth first
-      const childCoordinate = [startingCoordinate[0] + 2, startingCoordinate[1] + 1]
+      /* const childCoordinate = [startingCoordinate[0] + 2, startingCoordinate[1] + 1]
       const returnValues = knightMoves(childCoordinate, endingCoordinate, currentMoveCounter + 1, lowestMoveCounter, currentPath, shortestPath)
       lowestMoveCounter = returnValues[0]
-      shortestPath = returnValues[1]
+      shortestPath = returnValues[1] */
     }
     // check if down 2, right 1 is valid and not already visited
     if (
       startingCoordinate[0] + 1 <= 7 &&
       startingCoordinate[1] + 2 <= 7 &&
-      checkIfVisited(currentPath, [startingCoordinate[0] + 1, startingCoordinate[1] + 2]) === false /* &&
-      checkIfVisited(shortestPath, [startingCoordinate[0] + 1, startingCoordinate[1] + 2]) === false */
+      checkIfVisited(currentPath, [startingCoordinate[0] + 1, startingCoordinate[1] + 2]) === false &&
+      checkIfVisited(shortestPath, [startingCoordinate[0] + 1, startingCoordinate[1] + 2]) === false
     ) {
       // breadth first
-      /* if (endingCoordinate[0] === startingCoordinate[0] + 1 && endingCoordinate[1] === startingCoordinate[1] + 2) {
+      if (endingCoordinate[0] === startingCoordinate[0] + 1 && endingCoordinate[1] === startingCoordinate[1] + 2) {
         shortestPath.unshift([startingCoordinate[0] + 1, startingCoordinate[1] + 2])
         startingCoordinate = shortestPath[0]
         currentMoveCounter += 1
         return knightMoves(startingCoordinate, endingCoordinate, currentMoveCounter, lowestMoveCounter, currentPath, shortestPath)
       } 
-      shortestPath.push([startingCoordinate[0] + 1, startingCoordinate[1] + 2]) */
+      shortestPath.push([startingCoordinate[0] + 1, startingCoordinate[1] + 2])
 
       // depth first
-      const childCoordinate = [startingCoordinate[0] + 1, startingCoordinate[1] + 2]
+      /* const childCoordinate = [startingCoordinate[0] + 1, startingCoordinate[1] + 2]
       const returnValues = knightMoves(childCoordinate, endingCoordinate, currentMoveCounter + 1, lowestMoveCounter, currentPath, shortestPath)
       lowestMoveCounter = returnValues[0]
-      shortestPath = returnValues[1]
+      shortestPath = returnValues[1] */
     }
     // check if down 2, left 1 is valid and not already visited
     if (
       startingCoordinate[0] - 1 >= 0 &&
       startingCoordinate[1] + 2 <= 7 &&
-      checkIfVisited(currentPath, [startingCoordinate[0] - 1, startingCoordinate[1] + 2]) === false /* &&
-      checkIfVisited(shortestPath, [startingCoordinate[0] - 1, startingCoordinate[1] + 2]) === false */
+      checkIfVisited(currentPath, [startingCoordinate[0] - 1, startingCoordinate[1] + 2]) === false &&
+      checkIfVisited(shortestPath, [startingCoordinate[0] - 1, startingCoordinate[1] + 2]) === false
     ) {
       // breadth first
-      /* if (endingCoordinate[0] === startingCoordinate[0] - 1 && endingCoordinate[1] === startingCoordinate[1] + 2) {
+      if (endingCoordinate[0] === startingCoordinate[0] - 1 && endingCoordinate[1] === startingCoordinate[1] + 2) {
         shortestPath.unshift([startingCoordinate[0] - 1, startingCoordinate[1] + 2])
         startingCoordinate = shortestPath[0]
         currentMoveCounter += 1
         return knightMoves(startingCoordinate, endingCoordinate, currentMoveCounter, lowestMoveCounter, currentPath, shortestPath)
       } 
-      shortestPath.push([startingCoordinate[0] - 1, startingCoordinate[1] + 2]) */
+      shortestPath.push([startingCoordinate[0] - 1, startingCoordinate[1] + 2])
 
       // depth first
-      const childCoordinate = [startingCoordinate[0] - 1, startingCoordinate[1] + 2]
+      /* const childCoordinate = [startingCoordinate[0] - 1, startingCoordinate[1] + 2]
       const returnValues = knightMoves(childCoordinate, endingCoordinate, currentMoveCounter + 1, lowestMoveCounter, currentPath, shortestPath)
       lowestMoveCounter = returnValues[0]
-      shortestPath = returnValues[1]
+      shortestPath = returnValues[1] */
     }
     // check if left 2, down 1 is valid and not already visited
     if (
       startingCoordinate[0] - 2 >= 0 &&
       startingCoordinate[1] + 1 <= 7 &&
-      checkIfVisited(currentPath, [startingCoordinate[0] - 2, startingCoordinate[1] + 1]) === false /* &&
-      checkIfVisited(shortestPath, [startingCoordinate[0] - 2, startingCoordinate[1] + 1]) === false */
+      checkIfVisited(currentPath, [startingCoordinate[0] - 2, startingCoordinate[1] + 1]) === false &&
+      checkIfVisited(shortestPath, [startingCoordinate[0] - 2, startingCoordinate[1] + 1]) === false
     ) {
       // breadth first
-      /* if (endingCoordinate[0] === startingCoordinate[0] - 2 && endingCoordinate[1] === startingCoordinate[1] + 1) {
+      if (endingCoordinate[0] === startingCoordinate[0] - 2 && endingCoordinate[1] === startingCoordinate[1] + 1) {
         shortestPath.unshift([startingCoordinate[0] - 2, startingCoordinate[1] + 1])
         startingCoordinate = shortestPath[0]
         currentMoveCounter += 1
         return knightMoves(startingCoordinate, endingCoordinate, currentMoveCounter, lowestMoveCounter, currentPath, shortestPath)
       } 
-      shortestPath.push([startingCoordinate[0] - 2, startingCoordinate[1] + 1]) */
+      shortestPath.push([startingCoordinate[0] - 2, startingCoordinate[1] + 1])
 
       // depth first
-      const childCoordinate = [startingCoordinate[0] - 2, startingCoordinate[1] + 1]
+      /* const childCoordinate = [startingCoordinate[0] - 2, startingCoordinate[1] + 1]
       const returnValues = knightMoves(childCoordinate, endingCoordinate, currentMoveCounter + 1, lowestMoveCounter, currentPath, shortestPath)
       lowestMoveCounter = returnValues[0]
-      shortestPath = returnValues[1]
+      shortestPath = returnValues[1] */
     }
     // check if left 2, up 1 is valid and not already visited
     if (
       startingCoordinate[0] - 2 >= 0 &&
       startingCoordinate[1] - 1 >= 0 &&
-      checkIfVisited(currentPath, [startingCoordinate[0] - 2, startingCoordinate[1] - 1]) === false /* &&
-      checkIfVisited(shortestPath, [startingCoordinate[0] - 2, startingCoordinate[1] - 1]) === false */
+      checkIfVisited(currentPath, [startingCoordinate[0] - 2, startingCoordinate[1] - 1]) === false &&
+      checkIfVisited(shortestPath, [startingCoordinate[0] - 2, startingCoordinate[1] - 1]) === false
     ) {
       // breadth first
-      /* if (endingCoordinate[0] === startingCoordinate[0] - 2 && endingCoordinate[1] === startingCoordinate[1] - 1) {
+      if (endingCoordinate[0] === startingCoordinate[0] - 2 && endingCoordinate[1] === startingCoordinate[1] - 1) {
         shortestPath.unshift([startingCoordinate[0] - 2, startingCoordinate[1] - 1])
         startingCoordinate = shortestPath[0]
         currentMoveCounter += 1
         return knightMoves(startingCoordinate, endingCoordinate, currentMoveCounter, lowestMoveCounter, currentPath, shortestPath)
       }
-      shortestPath.push([startingCoordinate[0] - 2, startingCoordinate[1] - 1]) */
+      shortestPath.push([startingCoordinate[0] - 2, startingCoordinate[1] - 1])
 
       // depth first
-      const childCoordinate = [startingCoordinate[0] - 2, startingCoordinate[1] - 1]
+      /* const childCoordinate = [startingCoordinate[0] - 2, startingCoordinate[1] - 1]
       const returnValues = knightMoves(childCoordinate, endingCoordinate, currentMoveCounter + 1, lowestMoveCounter, currentPath, shortestPath)
       lowestMoveCounter = returnValues[0]
-      shortestPath = returnValues[1]
+      shortestPath = returnValues[1] */
     }
     // check if up 2, left 1 is valid and not already visited
     if (
       startingCoordinate[0] - 1 >= 0 &&
       startingCoordinate[1] - 2 >= 0 &&
-      checkIfVisited(currentPath, [startingCoordinate[0] - 1, startingCoordinate[1] - 2]) === false /* &&
-      checkIfVisited(shortestPath, [startingCoordinate[0] - 1, startingCoordinate[1] - 2]) === false */
+      checkIfVisited(currentPath, [startingCoordinate[0] - 1, startingCoordinate[1] - 2]) === false &&
+      checkIfVisited(shortestPath, [startingCoordinate[0] - 1, startingCoordinate[1] - 2]) === false
     ) {
-      /* if (endingCoordinate[0] === startingCoordinate[0] - 1 && endingCoordinate[1] === startingCoordinate[1] - 2) {
+      if (endingCoordinate[0] === startingCoordinate[0] - 1 && endingCoordinate[1] === startingCoordinate[1] - 2) {
         shortestPath.unshift([startingCoordinate[0] - 1, startingCoordinate[1] - 2])
         startingCoordinate = shortestPath[0]
         currentMoveCounter += 1
         return knightMoves(startingCoordinate, endingCoordinate, currentMoveCounter, lowestMoveCounter, currentPath, shortestPath)
       }
-      shortestPath.push([startingCoordinate[0] - 1, startingCoordinate[1] - 2]) */
+      shortestPath.push([startingCoordinate[0] - 1, startingCoordinate[1] - 2])
 
       // depth first
-      const childCoordinate = [startingCoordinate[0] - 1, startingCoordinate[1] - 2]
+      /* const childCoordinate = [startingCoordinate[0] - 1, startingCoordinate[1] - 2]
       const returnValues = knightMoves(childCoordinate, endingCoordinate, currentMoveCounter + 1, lowestMoveCounter, currentPath, shortestPath)
       lowestMoveCounter = returnValues[0]
-      shortestPath = returnValues[1]
+      shortestPath = returnValues[1] */
     }
     // check if up 2, right 1 is valid and not already visited
     if (
       startingCoordinate[0] + 1 <= 7 &&
       startingCoordinate[1] - 2 >= 0 &&
-      checkIfVisited(currentPath, [startingCoordinate[0] + 1, startingCoordinate[1] - 2]) === false /* &&
-      checkIfVisited(shortestPath, [startingCoordinate[0] + 1, startingCoordinate[1] - 2]) === false */
+      checkIfVisited(currentPath, [startingCoordinate[0] + 1, startingCoordinate[1] - 2]) === false &&
+      checkIfVisited(shortestPath, [startingCoordinate[0] + 1, startingCoordinate[1] - 2]) === false
     ) {
-      /* if (endingCoordinate[0] === startingCoordinate[0] + 1 && endingCoordinate[1] === startingCoordinate[1] - 2) {
+      if (endingCoordinate[0] === startingCoordinate[0] + 1 && endingCoordinate[1] === startingCoordinate[1] - 2) {
         shortestPath.unshift([startingCoordinate[0] + 1, startingCoordinate[1] - 2])
         startingCoordinate = shortestPath[0]
         currentMoveCounter += 1
         return knightMoves(startingCoordinate, endingCoordinate, currentMoveCounter, lowestMoveCounter, currentPath, shortestPath)
       }
-      shortestPath.push([startingCoordinate[0] + 1, startingCoordinate[1] - 2]) */
+      shortestPath.push([startingCoordinate[0] + 1, startingCoordinate[1] - 2])
 
       // depth first
-      const childCoordinate = [startingCoordinate[0] + 1, startingCoordinate[1] - 2]
+      /* const childCoordinate = [startingCoordinate[0] + 1, startingCoordinate[1] - 2]
       const returnValues = knightMoves(childCoordinate, endingCoordinate, currentMoveCounter + 1, lowestMoveCounter, currentPath, shortestPath)
       lowestMoveCounter = returnValues[0]
-      shortestPath = returnValues[1]
+      shortestPath = returnValues[1] */
     }
     // remove first item in queue since it was just evaluated
     /* traversalQueue.unshift() */ /* MOVE THIS TO THE TOP, this removes children that were just added, or children not visited yet. also SHOULD BE SHIFT */
-  }
+
     // breadth first
-    /* console.log(startingCoordinate, "old")
+    console.log(startingCoordinate, "old")
     startingCoordinate = shortestPath[0]
     console.log(startingCoordinate, "new")
     currentMoveCounter += 1
-    knightMoves(startingCoordinate, endingCoordinate, currentMoveCounter, lowestMoveCounter, currentPath, shortestPath) */
-    console.log(startingCoordinate, "starting")
-    console.log(currentPath, "current path")
-    console.log(shortestPath, "shortest path")
+    knightMoves(startingCoordinate, endingCoordinate, currentMoveCounter, lowestMoveCounter, currentPath, shortestPath)
+  }
+
+    // console.log(startingCoordinate, "starting")
+    // console.log(currentPath, "current path")
+    // console.log(shortestPath, "shortest path")
     // depth first
-    if (startingCoordinate[0] === shortestPath[0][0] && startingCoordinate[1] === shortestPath[0][1]) {
+    /* if (startingCoordinate[0] === shortestPath[0][0] && startingCoordinate[1] === shortestPath[0][1]) {
       console.log(`You made it in ${lowestMoveCounter} moves! Here's your path:`)
       shortestPath.forEach((coordinate) => {
         console.log(coordinate)
       })
     } else {
       return [lowestMoveCounter, shortestPath]
-    }
+    } */
 
 
     // if not the same
